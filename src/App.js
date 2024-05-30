c211715917f6af5fa2e0cc4cf3e2200e913a2c24
Kesav1997 <kesavbollam@gmail.com>
2024-05-29 22:30:53 -0400

    tweaked few copies on login and register page.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Login, Register, Home, ForgotPassword } from "./pages";
 
 const App = () => {
-  // Routes
+  // All the App pages Routes
   return (
     <Router>
       <Routes>
--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ if (!PUBLISHABLE_KEY) {
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
