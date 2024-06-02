155705c09b33f837bd177442509459ad2bfdf1df
Kesav1997 <kesavbollam@gmail.com>
2024-06-01 20:49:22 -0400

    Fixed a bug on routes file to fix the redirect issue.

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,29 +1,28 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Login, Register, Home, ForgotPassword } from "../pages";
 import { ProtectedRoute } from "../components";
-
-// Component for defining protected routes
-const ProtectedRoutes = () => {
-  return (
-    <>
-      {/* Protected route for the Home component */}
-      <ProtectedRoute path="/" element={<Home />} />
-    </>
-  );
-};
 
 // Component for defining all routes
 const AppRoutes = () => {
   return (
-    <Routes>
-      {/* Routes for protected pages */}
-      <Route element={<ProtectedRoutes />} />
+    <Router>
+      <Routes>
+        {/* Protected routes */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
 
-      {/* Routes for public pages */}
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-    </Routes>
+        {/* Public routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+      </Routes>
+    </Router>
   );
 };
 
